4bab55542a91e1cb3c0b64330ebc776ccafce963
Emelie <emeliie094@hotmail.com>
2022-01-31 14:18:47 +0100

    added project to my work

--- src/pages/MyWork.js
+++ src/pages/MyWork.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-scroll'
+import publicHolidaysImg from '../images/publicHolidays.png'
 
 function MyWork() {
     return (
@@ -7,10 +8,14 @@ function MyWork() {
             <h1>Mina Projekt</h1>
             <div className='wrapper'>
                 <div className="container">
-                    <div className='preview'></div>
+                    <div className='preview'>
+                        <img src={publicHolidaysImg} />
+                    </div>
                     <div>
-                        <button className="link-button">GitHub</button>
-                        <button className="link-button">Demo</button>
+
+
+                        <a href="https://github.com/Emeliie094/fetch-holidays" target="_blank"><button className="link-button">GitHub</button></a>
+                        <a href="https://emeliie094.github.io/fetch-holidays/" target="_blank"><button className="link-button">Demo</button></a>
                     </div>
                 </div>
                 <div className="container">
